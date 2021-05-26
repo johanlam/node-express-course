0b88a05ad1a3d1c8b4f8fc8682f5bdf9471eb72c
Johan Lam <johan.lam34@gmail.com>
2021-05-26 03:07:10 +0200

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const mockUserData = [{ name: 'Mark' }, { name: 'Jill' }];
 
 app.get('/users', (req, res) => {
@@ -12,6 +14,7 @@ app.get('/users', (req, res) => {
     });
 });
 
+// :id is treated as a variable
 app.get('/users/:id', (req, res) => {
     console.log(req.params.id);
     res.json({
@@ -19,6 +22,24 @@ app.get('/users/:id', (req, res) => {
         message: 'successfully got users. Nice!',
         user: req.params.id,
     });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here',
+        });
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match',
+        });
+    }
 });
 
 app.listen(8000, () => {
